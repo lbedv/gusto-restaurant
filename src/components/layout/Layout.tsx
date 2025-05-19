ce57ab82122547ee34daea3c254506b369978ee4
lebediev92 <lebediev92@gmail.com>
2025-05-19 16:35:07 +0200

    Add Footer

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
 import { Toaster } from 'sonner';
 
 type LayoutProps = {
@@ -13,6 +14,7 @@ const Layout = ({ children }: LayoutProps) => {
       <main className="flex-1">
         {children}
       </main>
+      <Footer />
       <Toaster position="top-right" />
     </div>
   );
