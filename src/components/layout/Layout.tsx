b675c2faf661794b422da8ad979c456a92a2031e
lebediev92 <lebediev92@gmail.com>
2025-05-19 16:15:38 +0200

    Add Header

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,13 +1,21 @@
-
 import React from 'react';
-
+import Header from './Header';
+import { Toaster } from 'sonner';
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 const Layout = ({ children }: LayoutProps) => {
-  return <></>;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-1">
+        {children}
+      </main>
+      <Toaster position="top-right" />
+    </div>
+  );
 };
 
 export default Layout;
